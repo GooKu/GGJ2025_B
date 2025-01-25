0a63f70bb0c3227a13c907c10d3c078b207c36c0
Goo <kuwoods@hotmail.com>
2025-01-25 15:01:46 +0800

    Implement StartGameSignal and setup start game call

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject badEndUI;
     [SerializeField] private GameObject goodEndUI;
+    [SerializeField] private PlayableDirector stagePlayer;
+
+    //call by button
+    public void PlayStartShow()
+    {
+        stagePlayer.Play();
+    }
+    //call by event
+    public void StartGame()
+    {
+        GameObject.FindAnyObjectByType<MoveUp>().enabled = true;
+        GameObject.FindAnyObjectByType<Clicker>().enabled = true;
+        GameObject.FindGameObjectWithTag(Tag.Player).GetComponent<Rigidbody2D>().simulated = true;
+    }
 
     public void GoodEnd()
     {
