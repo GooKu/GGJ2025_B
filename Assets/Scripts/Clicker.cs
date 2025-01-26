2ca0519fdda1fb021ad7bc2a056f965711b9f4d7
MuryanBKS <muryanbks+github@gmail.com>
2025-01-26 10:38:26 +0800

    delete update()

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -13,34 +13,17 @@ public class Clicker : MonoBehaviour
         player = GameObject.FindGameObjectWithTag(Tag.Player);
     }
 
-    void Update()
-    {
-        // 檢測滑鼠左鍵點擊
-        if (Input.GetMouseButtonDown(0))
-        {
-            // 取得滑鼠在世界中的位置
-            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // 確保 Z 軸為角色的 Z 軸（2D 遊戲通常使用相同的 Z 軸）
-            mouseWorldPosition.z = player.transform.position.z;
-
-            // 判斷滑鼠點擊是在角色的左邊還是右邊
-            if (mouseWorldPosition.x < player.transform.position.x)
-            {
-                // 滑鼠點擊在角色的左邊
-                OnClick(true);
-            }
-            else
-            {
-                // 滑鼠點擊在角色的右邊
-                OnClick(false);
-            }
-        }
-    }
-
     public void OnClick(bool isLeft)
     {
         if (!enabled) { return; }
+
+        // 取得滑鼠在世界中的位置
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 確保 Z 軸為角色的 Z 軸（2D 遊戲通常使用相同的 Z 軸）
+        mouseWorldPosition.z = player.transform.position.z;
+
+        isLeft = mouseWorldPosition.x < player.transform.position.x;
 
         var power = isLeft ? movePower : -movePower;
         player.GetComponent<Rigidbody2D>().AddForceX(power, ForceMode2D.Impulse);
